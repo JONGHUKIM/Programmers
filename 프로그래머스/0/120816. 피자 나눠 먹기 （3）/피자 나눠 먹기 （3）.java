b6e7c2ca735fb442ff9f71789b6e4569d9f5d900
JONGHUKIM <hotsky803@gmail.com>
2025-08-04 17:28:54 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        for (int i = 1; ;i++) {
+            if ((i * slice) >= n) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
