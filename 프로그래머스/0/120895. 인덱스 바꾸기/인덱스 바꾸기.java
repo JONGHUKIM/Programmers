5613b4d95da030e9ea5c67424340d51278339297
JONGHUKIM <hotsky803@gmail.com>
2025-08-05 18:59:45 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder answer = new StringBuilder(my_string);
+        
+        char ch1 = my_string.charAt(num1);
+        char ch2 = my_string.charAt(num2);
+        
+        answer.setCharAt(num1, ch2);
+        answer.setCharAt(num2, ch1);
+        
+        return answer.toString();
+    }
+}
