0fcb69b978c6904fab102fca3db83d5a1dbe75c1
JONGHUKIM <hotsky803@gmail.com>
2025-08-05 20:06:22 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String solution(int age) {
+        String strAge = String.valueOf(age);
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : strAge.toCharArray()) {
+            switch(c) {
+                case '0':
+                    sb.append('a');
+                    break;
+                case '1':
+                    sb.append('b');
+                    break;
+                case '2':
+                    sb.append('c');
+                    break;
+                case '3':
+                    sb.append('d');
+                    break;
+                case '4':
+                    sb.append('e');
+                    break;
+                case '5':
+                    sb.append('f');
+                    break;
+                case '6':
+                    sb.append('g');
+                    break;
+                case '7':
+                    sb.append('h');
+                    break;
+                case '8':
+                    sb.append('i');
+                    break;
+                case '9':
+                    sb.append('j');
+                    break;
+                default:
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+}
