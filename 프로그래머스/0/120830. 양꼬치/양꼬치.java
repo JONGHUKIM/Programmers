504ed0dee2f0524c09cb0411b0f670f2ff20651a
JONGHUKIM <hotsky803@gmail.com>
2025-08-21 15:09:53 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,7 +1,9 @@
+// 복습
+
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        int serv_drink = (n / 10) * 2000;  
+        int serv_drink = (n / 10) * 2000;
         
         answer = (n * 12000) + (k * 2000) - serv_drink;
         
