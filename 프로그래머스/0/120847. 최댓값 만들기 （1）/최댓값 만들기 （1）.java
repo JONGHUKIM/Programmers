19f5340e1458a9a542c06ff05498a01af093d0e2
JONGHUKIM <hotsky803@gmail.com>
2025-08-22 09:54:57 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.43 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,13 +1,15 @@
+// 복습
+
 import java.util.Arrays;
 
 class Solution {
     public int solution(int[] numbers) {
-        
+
         Arrays.sort(numbers);
         
         int n = numbers.length;
         
-        int product1 = numbers[n - 1] * numbers[n - 2];
+        int product1 = numbers[n - 1] * numbers [n - 2];
         int product2 = numbers[0] * numbers[1];
         
         return Math.max(product1, product2);
