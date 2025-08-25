c1a9b66a70aa325fc358a5f75e897888528d6108
JONGHUKIM <hotsky803@gmail.com>
2025-08-25 17:43:59 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,19 +1,16 @@
+// 복습
+
 class Solution {
     public int solution(int n) {
-        
         int answer = 0;
         
-        // while 문
-        
-        // while (0 < n) {
-            // answer += n % 10;
-            // n = n / 10;
-        // }
-        
-        // for 문
-        
-        for (; 0 < n; n /= 10) {
+        for (; n > 0; n /= 10) {
             answer += n % 10;
+        }
+        
+        while(n > 0) {
+            answer += n % 10;
+            n = n / 10;
         }
         
         return answer;
