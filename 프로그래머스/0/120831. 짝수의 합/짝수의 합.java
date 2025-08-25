3a1de2652947ffb3012532da0a8d0991c07a30c4
JONGHUKIM <hotsky803@gmail.com>
2025-08-25 17:45:39 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,15 +1,13 @@
+// 복습
+
 class Solution {
     public int solution(int n) {
-        
         int answer = 0;
         
         for (int i = 2; i <= n; i+=2) {
-            if (0 < i && i <= 1000) {
-                answer += i;
-            }
+            answer += i;
         }
+        
         return answer;
     }
-    
-    
 }
