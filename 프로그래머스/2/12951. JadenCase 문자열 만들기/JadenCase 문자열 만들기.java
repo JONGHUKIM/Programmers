4dc70d352dec8dfa67486b64bacc0f6b4d052789
JONGHUKIM <hotsky803@gmail.com>
2025-08-26 18:53:07 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.22 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s) {
+        String[] words = s.toLowerCase().split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for ( String word : words) {
+            if (word.length() > 0) {
+                String firstChar = word.substring(0, 1);
+                String rest = word.substring(1);
+                
+                sb.append(firstChar.toUpperCase()).append(rest).append(" ");
+            } else {
+                sb.append(" ");
+            }
+        }
+        
+        if (s.endsWith(" ")) {
+            return sb.toString();
+        } else {
+            return sb.toString().trim();
+        }
+    }
+}
