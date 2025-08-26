d2710324e0efa296680595036a872900670151d2
JONGHUKIM <hotsky803@gmail.com>
2025-08-26 17:22:05 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,13 +1,12 @@
+// 복습
+
 class Solution {
     public int[] solution(int[] num_list) {
-        
         for (int i = 0; i < num_list.length / 2; i++) {
             int array = num_list[i];
             
-            num_list[i] = num_list[num_list.length - 1 -i];
-            
-            num_list[num_list.length - 1 -i] = array;
-            
+            num_list[i] = num_list[num_list.length - 1 - i];
+            num_list[num_list.length - 1 - i] = array;
         }
         
         return num_list;
