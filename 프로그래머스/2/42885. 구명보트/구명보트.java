3f7f71a825543f43a0822b3fb89ba7f24ac2a180
JONGHUKIM <hotsky803@gmail.com>
2025-08-27 17:33:01 +0900

    [level 2] Title: 구명보트, Time: 12.38 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int boat = 0;
+        int left = 0;
+        int right = people.length - 1;
+        
+        while (left <= right) {
+            boat++;
+            
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+        }
+        return boat;
+    }
+}
