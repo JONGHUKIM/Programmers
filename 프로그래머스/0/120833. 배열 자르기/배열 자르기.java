ec20c4c2184769541f87a1383686fa4f00c1f043
JONGHUKIM <hotsky803@gmail.com>
2025-08-28 17:34:11 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int length = num2 - num1 + 1;
+        int len = num2 - num1 + 1;
+        int[] answer = new int[len];
         
-        int[] answer = new int[length];
-        
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < len; i++) {
             answer[i] = numbers[num1 + i];
         }
         
