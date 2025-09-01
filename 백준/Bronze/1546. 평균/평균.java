10603a9e1c153fdfb745afa44d9ae6e66ad996e3
JONGHUKIM <hotsky803@gmail.com>
2025-09-01 15:11:09 +0900

    [Bronze I] Title: 평균, Time: 228 ms, Memory: 18692 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,22 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
+        double avg = 0;
+        int max = 0;
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
         
-        long sum = 0;
-        long max = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if ( a[i] > max) {
+        for (int i = 0; i < a.length; i++) {
+            if ( a[i] > max ) {
                 max = a[i];
             }
-            sum += a[i];
+            avg += a[i];
         }
         
-        System.out.println(sum*100.0/max/n);
+        System.out.println(avg * 100 / max / n);
         
+        sc.close();
     }
 }
