958d0a2c160aa9f9cba30e9fdeef8699c988aa04
JONGHUKIM <hotsky803@gmail.com>
2025-09-01 17:05:40 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1348 ms, Memory: 128720 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+// 1 2 3 4
+// 2 3 4 5
+// 3 4 5 6
+// 4 5 6 7
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = 
+            new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long d[][] = new long [n + 1][n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                d[i][j] = d[i][j-1] + d[i-1][j] - d[i-1][j-1] 
+                    + Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int q = 0; q < m; q++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(d[x2][y2] - d[x1-1][y2] - d[x2][y1-1] + d[x1-1][y1-1]);
+        }
+        
+        br.close();
+    }
+}
