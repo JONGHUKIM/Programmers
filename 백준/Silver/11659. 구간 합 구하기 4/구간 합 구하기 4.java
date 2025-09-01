797bea09127b2ececfb0729eb3d8b67172b43f80
JONGHUKIM <hotsky803@gmail.com>
2025-09-01 15:40:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1172 ms, Memory: 59376 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,28 +1,30 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = 
+        BufferedReader br = 
             new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = 
-            new StringTokenizer(bufferedReader.readLine());
+        StringTokenizer st = 
+            new StringTokenizer(br.readLine());
         
-        int suNo = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
-        long[] S = new long[suNo + 1];
+        int suNo = Integer.parseInt(st.nextToken());
+        int quizNo = Integer.parseInt(st.nextToken());
+        long[] s = new long[suNo + 1];
         
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= suNo; i++) {
-            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+            s[i] = s[i - 1] + Integer.parseInt(st.nextToken());
         }
-
+        
         for (int q = 0; q < quizNo; q++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j] - S[i - 1]);
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            System.out.println(s[j] - s[i - 1]);
         }
+        
+        br.close();
+        
     }
 }
