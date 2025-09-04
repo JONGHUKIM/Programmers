60122f57fcf0fadb9ec34eab4eb6ca0a4bab54a5
JONGHUKIM <hotsky803@gmail.com>
2025-09-04 18:07:49 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16424 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -3,9 +3,9 @@ import java.io.*;
 
 public class Main {
     public static void main (String[] args) throws IOException {
-        BufferedReader br = 
+        BufferedReader br =
             new BufferedReader (new InputStreamReader(System.in));
-        StringTokenizer st = 
+        StringTokenizer st =
             new StringTokenizer (br.readLine());
         
         int n = Integer.parseInt(st.nextToken());
@@ -13,29 +13,30 @@ public class Main {
         st = new StringTokenizer (br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int[] a = new int[n];
-        int count = 0;
         
         st = new StringTokenizer (br.readLine());
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
+        
         Arrays.sort(a);
+        
         int i = 0;
         int j = n - 1;
+        int count = 0;
+        
         while (i < j) {
-            if (a[i] + a[j] < m) {
-                i++;
-            } else if (a[i] + a[j] > m) {
+            if (a[i] + a[j] > m) {
                 j--;
+            } else if (a[i] + a[j] < m) {
+                i++;
             } else {
                 count++;
                 i++;
                 j--;
             }
         }
-        
         System.out.println(count);
         br.close();
     }
 }
-
