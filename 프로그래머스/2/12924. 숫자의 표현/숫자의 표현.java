196f563fdd5797a71cda08b7ab433a9360203785
JONGHUKIM <hotsky803@gmail.com>
2025-09-05 12:22:10 +0900

    [level 2] Title: 숫자의 표현, Time: 0.56 ms, Memory: 53.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int count = 1;
+        int sum = 1;
+        int start = 1;
+        int end = 1;
+        
+        while (end != n) {
+            if (sum == n) {
+                count++;
+                end++;
+                sum = sum + end;
+            } else if (sum > n) {
+                sum = sum - start;
+                start++;
+            } else {
+                end++;
+                sum = sum + end;
+            }
+        }
+        return count;
+    }
+}
