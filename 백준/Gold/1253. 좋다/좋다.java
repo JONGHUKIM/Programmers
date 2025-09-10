bce9b62504b9f6ac33c67db1ba6025368aedab5d
JONGHUKIM <hotsky803@gmail.com>
2025-09-10 11:38:26 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,25 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br =
-            new BufferedReader (new InputStreamReader(System.in));
+            new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
         
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st =
+            new StringTokenizer(br.readLine());
+        
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
-        } 
+        }
         
         Arrays.sort(a);
         int count = 0;
         
         for (int i = 0; i < n; i++) {
-            int target = a[i];
             int left = 0;
             int right = n - 1;
+            int t = a[i];
             
             while (left < right) {
                 if (left == i) {
@@ -31,18 +33,18 @@ public class Main {
                     right--;
                     continue;
                 }
-                
                 int sum = a[left] + a[right];
-                if (sum == target) {
+                if (sum == t) {
                     count++;
                     break;
-                } else if (sum < target) {
+                } else if (sum < t) {
                     left++;
                 } else {
                     right--;
                 }
             }
         }
+        
         System.out.println(count);
         br.close();
     }
