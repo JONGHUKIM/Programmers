6722742ae7ebd96c7dbfb6ace35ac5205e9656be
JONGHUKIM <hotsky803@gmail.com>
2025-09-10 18:31:23 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2280 ms, Memory: 619840 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -3,26 +3,24 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =
+        BufferedReader br = 
             new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = 
+        BufferedWriter bw =
             new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st =
             new StringTokenizer(br.readLine());
         
-        int n = Integer.parseInt(st.nextToken()); // 숫자 데이터
-        int l = Integer.parseInt(st.nextToken()); // 윈도우 크기
-        Deque<Node> myDeque = new LinkedList<>(); // 덱 선언
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        Deque<Node> myDeque = new LinkedList<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             int now = Integer.parseInt(st.nextToken());
-            // 마지막이 비지 않거나 새로운 값이 들어올 때마다 현재 수보다 큰 값을 덱에서 제거
-            while (!myDeque.isEmpty() && myDeque.getLast().value > now) {
+            while(!myDeque.isEmpty()&&myDeque.getLast().value > now) {
                 myDeque.removeLast();
             }
             myDeque.addLast(new Node(now, i));
             
-            // 윈도우의 범위를 벗어난 값은 덱에서 제거
             if (myDeque.getFirst().index <= i - l) {
                 myDeque.removeFirst();
             }
@@ -33,12 +31,10 @@ public class Main {
         br.close();
     }
     
-    // 노드 클래스
     private static class Node {
         int value;
         int index;
         
-        // 노드 생성자
         Node(int value, int index) {
             this.value = value;
             this.index = index;
