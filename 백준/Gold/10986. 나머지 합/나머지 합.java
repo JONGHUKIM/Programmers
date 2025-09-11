696719c6400856bc5734bbfcaed3e0b73e8e004d
JONGHUKIM <hotsky803@gmail.com>
2025-09-11 15:19:38 +0900

    [Gold III] Title: 나머지 합, Time: 456 ms, Memory: 120776 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = 
+        StringTokenizer st =
             new StringTokenizer(br.readLine());
         
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        long[] s = new long[n+1];
+        long[] s = new long[n + 1];
         long[] c = new long[m];
-        long answer = 0;
+        long answer = 0L;
         
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
@@ -23,17 +23,13 @@ public class Main {
             if (remainder == 0) {
                 answer++;
             }
-            
             c[remainder]++;
         }
-        
         for (int i = 0; i < m; i++) {
-            answer = answer + (c[i] * (c[i] - 1)) / 2;
+            answer = answer + (c[i] * (c[i] - 1) / 2);
         }
         
         System.out.println(answer);
-        
         br.close();
-        
     }
 }
