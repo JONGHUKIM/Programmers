53a89813f191ae2ba8a924ee7c466f97b21538b7
JONGHUKIM <hotsky803@gmail.com>
2025-09-12 15:25:43 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.07 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 0.86 ms
+메모리: 84.8 MB, 시간: 1.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 11일 18:33:43
+2025년 09월 12일 15:25:40
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,14 +1,16 @@
+// 복습
+
 import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int scores = 0; // 2점씩 증가
+        int scores = 0;
         Stack<Integer> stack = new Stack<>();
         
         for (int move : moves) {
-            int col = move - 1; // 인덱스가 0으로 변환
+            int col = move - 1;
             for (int row = 0; row < board.length; row++) {
-                if(board[row][col] != 0) {
+                if (board[row][col] != 0) {
                     int doll = board[row][col];
                     board[row][col] = 0;
                     
@@ -22,7 +24,6 @@ class Solution {
                 }
             }
         }
-        
         return scores;
     }
 }
