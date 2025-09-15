92682cb882a307c5a55d8d6013fb01de2083d3c4
JONGHUKIM <hotsky803@gmail.com>
2025-09-15 13:57:48 +0900

    [Silver II] Title: DNA 비밀번호, Time: 272 ms, Memory: 22936 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,10 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
     // 전역변수 선언
-    static int[] myArr;
     static int[] checkArr;
+    static int[] myArr;
     static int checkSecret;
     
     public static void main(String[] args) throws IOException {
@@ -16,30 +15,27 @@ public class Main {
         
         int s = Integer.parseInt(st.nextToken()); // 문자열 크기
         int p = Integer.parseInt(st.nextToken()); // 부분 문자열 크기
-        char[] a; // 문자열 데이터
         checkArr = new int[4]; // 비밀번호 배열
-        myArr = new int[4]; // 현재 문자 배열
-        checkSecret = 0; // checkSecert == 4, result++; 조건에 맞는지 검사 
-        int result = 0; // 정답
+        myArr = new int[4]; // 현재 배열
+        checkSecret = 0; // myArr과 checkArr이 같으면 ++ 또는 --;
+        int result = 0; // checkSecret == 4, ++ 정답
+        char[] a; // 문자열 데이터
         
-        // 문자 데이터를 입력 받음
-        a = br.readLine().toCharArray();
-        // 비밀번호 데이터를 입력 받음
+        a = br.readLine().toCharArray(); // 문자열을 읽어와 저장
         st = new StringTokenizer(br.readLine());
+        // 비밀번호 배열을 받음 
         for (int i = 0; i < 4; i++) {
             checkArr[i] = Integer.parseInt(st.nextToken());
-            // 0이면 조건에 충족, checkSecret++;
-            if (checkArr[i] == 0) {
-                checkSecret++;
+            if (checkArr[i] == 0) { // 0이면 이미 만족하기 때문에 checkSecret++;
+                checkSecret++; 
             }
         }
         
-        // 초기 슬라이딩 윈도우, 초기 부분 문자열(p) 처리
+        // 초기 P 부분 문자열을 처리, 초기 슬라이딩 윈도우
         for (int i = 0; i < p; i++) {
             Add(a[i]);
         }
         
-        // checkSecret == 4, result++
         if (checkSecret == 4) {
             result++;
         }
@@ -47,17 +43,18 @@ public class Main {
         // 슬라이딩 윈도우
         for (int i = p; i < s; i++) {
             Add(a[i]);
-            Remove(a[i-p]);
+            Remove(a[i - p]);
             if (checkSecret == 4) {
                 result++;
             }
         }
         System.out.println(result);
         br.close();
+        
     }
     
-    // 제거 되는 문자를 처리하는 Remove 함수 선언
-    private static void Remove(char c) {
+    // Remove 함수 선언 (제거되는 문자를 처리하는 함수)
+    private static void Remove (char c) {
         switch (c) {
             case 'A':
                 if (myArr[0] == checkArr[0]) {
@@ -86,8 +83,8 @@ public class Main {
         }
     }
     
-    // 추가 되는 문자를 처리하는 Add 함수 선언
-    private static void Add(char c) {
+    // Add 함수 선언 ( 새로 들어온 문자를 처리하는 함수)
+    private static void Add (char c) {
         switch (c) {
             case 'A':
                 myArr[0]++;
