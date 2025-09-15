b57518c6bdcb92ce0ee6ae0edc432d40564df8aa
JONGHUKIM <hotsky803@gmail.com>
2025-09-15 18:20:40 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 45652 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =
+            new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> que = new LinkedList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            que.add(i);
+        }
+        
+        while (que.size() > 1) {
+            que.poll();
+            if(!que.isEmpty()) {
+                que.add(que.poll());
+            }
+        }
+        if (!que.isEmpty()) {
+            bw.write(String.valueOf(que.poll()));
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
