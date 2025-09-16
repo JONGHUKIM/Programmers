fae37215a04e636d49a88e616c9c2d0e21926275
JONGHUKIM <hotsky803@gmail.com>
2025-09-16 17:19:16 +0900

    [Silver I] Title: 절댓값 힙, Time: 476 ms, Memory: 26584 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,20 +7,20 @@ public class Main {
             new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {            
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-            // 절대 값이 같은 경우 음수 우선
-            if (first_abs == second_abs) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int first = Math.abs(o1);
+            int second = Math.abs(o2);
+            if (first == second) { // 절대값이 같은 경우 음수 우선
                 return o1 > o2 ? 1 : -1;
             }
-            return first_abs - second_abs; // 절대 값이 작은 수
+            return first - second; // 절대값이 적은 수 우선
         });
+        
         for (int i = 0; i < n; i++) {
             int request = Integer.parseInt(br.readLine());
             if (request == 0) {
                 if (pq.isEmpty()) {
-                    System.out.println("0");
+                    System.out.println(0);
                 } else {
                     System.out.println(pq.poll());
                 }
@@ -28,6 +28,5 @@ public class Main {
                 pq.add(request);
             }
         }
-        br.close();
     }
 }
