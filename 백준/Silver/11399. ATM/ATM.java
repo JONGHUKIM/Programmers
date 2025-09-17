877e85d13d63cf661a01693cdd94e1329f5a878f
JONGHUKIM <hotsky803@gmail.com>
2025-09-17 18:05:38 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 15664 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -20,11 +20,11 @@ public class Main {
             int insert_index = i;
             int insert_value = a[i];
             for (int j = i - 1; j >= 0; j--) {
-                if (a[j] < a[i]) {
+                if(a[j] < a[i]) {
                     insert_index = j + 1;
                     break;
                 }
-                if( j == 0) {
+                if (j == 0) {
                     insert_index = 0;
                 }
             }
@@ -35,11 +35,11 @@ public class Main {
         }
         s[0] = a[0];
         for (int i = 1; i < n; i++) {
-            s[i] = s[i - 1] + a[i];
-        }
+            s[i] = s[i-1] + a[i];
+        } 
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum = sum + s[i];
+            sum += s[i];
         }
         System.out.println(sum);
         br.close();
