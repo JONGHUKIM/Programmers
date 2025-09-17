3054e329fb972b872b5f2f40d4cef966707b6ea0
JONGHUKIM <hotsky803@gmail.com>
2025-09-17 17:00:26 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,20 +1,46 @@
 import java.util.*;
 import java.io.*;
 
+// 선택정렬
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
         
-        StringBuilder sb = new StringBuilder();
         String s = br.readLine();
-        char[] c = s.toCharArray();
+        int[] a = new int[s.length()];
         
-        Arrays.sort(c);
-        for (int i = c.length - 1; i >= 0; i--) {
-            sb.append(c[i]);
+        for (int i = 0; i < s.length(); i++) {
+            a[i] = Integer.parseInt(s.substring(i, i + 1));
         }
-        System.out.println(sb.toString());
+        for (int i = 0; i < s.length(); i++) {
+            int max = i;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (a[j] > a[max]) {
+                    max = j; // 인덱스 값만 찾음
+                }
+            }
+            if (a[i] < a[max]) {
+                int temp = a[i];
+                a[i] = a[max];
+                a[max] = temp;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(a[i]);
+        }
         br.close();
     }
 }
+
+// String s = br.readLine();
+// char[] c = s.toCharArray();
+
+// StringBuilder sb = new StringBuilder();
+// Arrays.sort(c);
+// for (int i = c.length - 1; i >= 0; i--) {
+//    sb.append(c[i]);
+// }
+
+// System.out.println(sb.toString());
+// br.close();
