c1bef0aec3338940616627d169b691075a7cab8c
JONGHUKIM <hotsky803@gmail.com>
2025-09-17 18:10:41 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,43 +5,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =
+            new BufferedWriter(new OutputStreamWriter(System.out));
         
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
+        int sum = 0;
         int[] s = new int[n];
         
-        StringTokenizer st =
+        StringTokenizer st = 
             new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
         
-        for (int i = 0; i < n; i++) {
-            int insert_index = i;
-            int insert_value = a[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if(a[j] < a[i]) {
-                    insert_index = j + 1;
-                    break;
-                }
-                if (j == 0) {
-                    insert_index = 0;
-                }
-            }
-            for (int j = i; j > insert_index; j--) {
-                a[j] = a[j-1];
-            }
-            a[insert_index] = insert_value;
-        }
+        Arrays.sort(a);
         s[0] = a[0];
         for (int i = 1; i < n; i++) {
             s[i] = s[i-1] + a[i];
-        } 
-        int sum = 0;
+        }
         for (int i = 0; i < n; i++) {
             sum += s[i];
         }
-        System.out.println(sum);
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
