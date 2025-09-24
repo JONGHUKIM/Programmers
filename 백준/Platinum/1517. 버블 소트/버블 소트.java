dae1c6f4a2e4f320bf7a1b510ebcb273d6776570
JONGHUKIM <hotsky803@gmail.com>
2025-09-24 11:19:19 +0900

    [Platinum V] Title: 버블 소트, Time: 472 ms, Memory: 72528 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -6,42 +6,48 @@ public class Main {
     static long count;
     
     private static void mergeSort(int[] arr) {
-        int[] temp = new int[arr.length];
-        
-        mergeSort(arr, temp, 0, arr.length - 1);
+        int[] tem = new int[arr.length]; // 머지소트를 위한 임시배열
+        mergeSort(arr, tem, 0, arr.length - 1);
     }
     
-    private static void mergeSort(int[] arr, int[] temp, int start, int end) {
+    // mergeSort 재귀함수 생성
+    private static void mergeSort(int[] arr, int[] tem, int start, int end) {
         if (start < end) {
-            int mid = start + (end - start) / 2;
-            mergeSort(arr, temp, start, mid);
-            mergeSort(arr, temp, mid + 1, end);
-            merge(arr, temp , start, mid, end);
+            int mid = start + (end - start) / 2; // 오버 플로우 방지
+            mergeSort(arr, tem, start, mid);
+            mergeSort(arr, tem, mid + 1, end);
+            merge(arr, tem, start, mid, end);
         }
     }
     
-    private static void merge(int[] arr, int[] temp, int start, int mid, int end) {
+    // merge 재귀함수 생성
+    private static void merge(int[] arr, int[] tem, int start, int mid, int end) {
+        // 임시배열에 복사
         for (int i = start; i <= end; i++) {
-            temp[i] = arr[i];
+            tem[i] = arr[i];
         }
         
+        // 시작 포인터 설정
         int part1 = start;
         int part2 = mid + 1;
+        // 현재 저장할 인덱스 설정
         int index = start;
         
+        // 시작 포인터가 끝 포인터에 도달할 때까지 반복
         while (part1 <= mid && part2 <= end) {
-            if (temp[part1] <= temp[part2]) {
-                arr[index] = temp[part1];
+            if (tem[part1] <= tem[part2]) {
+                arr[index] = tem[part1];
                 part1++;
             } else {
-                arr[index] = temp[part2];
+                arr[index] = tem[part2];
                 part2++;
-                count += (mid - part1 + 1); // 여기서 count ++
+                count += (mid - part1 + 1); // 여기서 count++
             }
             index++;
         }
+        // 앞의 데이터가 남으면 채워줌
         for (int i = 0; i <= mid - part1; i++) {
-            arr[index + i] = temp[part1 + i];
+            arr[index + i] = tem[part1 + i]; 
         }
     }
     
@@ -52,8 +58,9 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
         
-        StringTokenizer st =
+        StringTokenizer st = 
             new StringTokenizer(br.readLine());
+        
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
