234eeef70615b1ad1fcddad308fd92409b5d3c62
JONGHUKIM <hotsky803@gmail.com>
2025-09-30 17:02:34 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
- 
+        
         int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
         int max = 0;
-        int sum = 0;
+        int sum = 0;        
+        int[] a = new int[n];
         double result = 0.0;
         
         StringTokenizer st =
@@ -21,9 +21,8 @@ public class Main {
             }
             sum += a[i];
         }
-        
         result = sum * 100.0 / max / n;
-        
         System.out.println(result);
+        br.close();
     }
 }
