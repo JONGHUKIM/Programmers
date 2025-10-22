2f9dccc0d8c7e9a56d5cc78b39018d347b325b0e
JONGHUKIM <hotsky803@gmail.com>
2025-10-22 10:51:46 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,15 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw =
-            new BufferedWriter(new OutputStreamWriter(System.out));
         
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
         int sum = 0;
-        int[] s = new int[n];
+        long[] s = new long[n];
         
-        StringTokenizer st = 
+        StringTokenizer st =
             new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
@@ -22,14 +20,14 @@ public class Main {
         Arrays.sort(a);
         s[0] = a[0];
         for (int i = 1; i < n; i++) {
-            s[i] = s[i-1] + a[i];
+            s[i] = s[i - 1] + a[i];
         }
+        
         for (int i = 0; i < n; i++) {
             sum += s[i];
         }
-        bw.write(String.valueOf(sum));
-        bw.flush();
-        bw.close();
+        
+        System.out.println(sum);
         br.close();
     }
 }
