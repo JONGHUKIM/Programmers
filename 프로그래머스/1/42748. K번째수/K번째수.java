bf7c90c225ccdf8b5ba661ccfccea040d92d9b9b
JONGHUKIM <hotsky803@gmail.com>
2025-10-23 13:39:01 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
 
+// 복습
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0] - 1; // 자바 배열 기준
+            int start = commands[i][0] - 1;
             int end = commands[i][1];
-            int k = commands[i][2] - 1; // 자바 배열 기준
+            int k = commands[i][2] - 1;
             
             int[] temp = Arrays.copyOfRange(array, start, end);
             Arrays.sort(temp);
