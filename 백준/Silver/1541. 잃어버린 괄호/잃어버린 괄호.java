0eb61a112a512f939d32bbebab2369d380c45763
JONGHUKIM <hotsky803@gmail.com>
2025-10-23 12:53:44 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,30 +4,27 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+            new BufferedReader(new InputStreamReader(System.in))           ;
         
         String s = br.readLine();
         String[] parts = s.split("-");
-        int result = sumGroup(parts[0]);
+        
+        String[] first = parts[0].split("\\+");
+        int result = 0;
+        for (String num : first) {
+            if (!num.isEmpty()) result += Integer.parseInt(num);
+        }
         
         for (int i = 1; i < parts.length; i++) {
-            result -= sumGroup(parts[i]);
+            String[] nums = parts[i].split("\\+");
+            int temp = 0;
+            for (String num : nums) {
+                if (!num.isEmpty()) temp += Integer.parseInt(num);
+            }
+            result -= temp;
         }
         
         System.out.println(result);
         br.close();
-    }
-    
-    private static int sumGroup(String group) {
-        String[] tokens = group.split("\\+");
-        int sum = 0;
-        
-        for (String t : tokens) {
-            if (!t.isEmpty()) {
-                sum += Integer.parseInt(t);
-            }
-        }
-        
-        return sum;
     }
 }
