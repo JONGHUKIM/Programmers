9ec856be135907b0a8dc722fe4b516b99fd7e0f3
JONGHUKIM <hotsky803@gmail.com>
2025-10-23 14:36:03 +0900

    [level 2] Title: 가장 큰 수, Time: 218.71 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,29 +1,26 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String solution(int[] numbers) {
-        
         String[] arr = new String[numbers.length];
-        for (int i = 0; i < arr.length; i++) {
+        
+        for (int i = 0; i < numbers.length; i++) {
             arr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return (s2+s1).compareTo(s1+s2);
-            }
-        });
+        Arrays.sort(arr, (a, b) -> compareNum(a, b));
         
         if (arr[0].equals("0")) {
             return "0";
         }
         
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i]);
-        }
+        return String.join("", arr);
+    }
+    
+    private static int compareNum(String a, String b) {
+        String ab = a + b;
+        String ba = b + a;
         
-        return sb.toString();
+        return ba.compareTo(ab);
     }
 }
