77be59122f170858e3f68c8d8a36137061d910ce
JONGHUKIM <hotsky803@gmail.com>
2025-10-30 14:14:59 +0900

    [level 1] Title: 모의고사, Time: 4.60 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,41 @@
+// 복습
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        
-        // 수포자들의 패턴(하드코딩)
+        // 수포자들의 패턴
         int[][] pattern = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
         
-        // 수포자들의 점수를 저장할 배열
+        // 수포자들의 점수를 저장할 배열 선언
         int[] scores = new int[3];
         
-        // 수포자들의 패턴과 정답이 일치하는지 확인
-        for (int i = 0; i < answers.length; i++) { // 정답 배열
-            for (int j = 0; j < pattern.length; j++) { // 수포자 배열
+        // 정답과 패턴이 일치하는지 확인
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < pattern.length; j++) {
                 if (answers[i] == pattern[j][i % pattern[j].length]) {
                     scores[j]++;
                 }
             }
         }
         
-        // 가장 높은 점수 저장
+        // 가장 높은 점수를 찾음
         int maxScore = Arrays.stream(scores).max().getAsInt();
-        // 가장 높은 점수를 가진 수포자들의 번호를 찾아서 리스트에 담음
-        ArrayList<Integer> answer = new ArrayList<>();
+        
+        // 수포자 중 가장 높은 점수를 받은 수포자를 리스트에 추가
+        ArrayList<Integer> score = new ArrayList<>();
         for (int i = 0; i < scores.length; i++) {
             if (scores[i] == maxScore) {
-                answer.add(i + 1);
+                score.add(i + 1);
             }
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        
+        // 결과 값 출력
+        return score.stream().mapToInt(Integer::intValue).toArray();
+        
     }
 }
