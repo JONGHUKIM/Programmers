533adad3c6addbb5a83920e80dff4b538c0bc9a1
JONGHUKIM <hotsky803@gmail.com>
2025-10-31 15:53:43 +0900

    [level 2] Title: 방문 길이, Time: 42.97 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 class Solution {
     
-    // 좌표 평면을 벗어나는지 체크하는 메서드
+    // 좌표를 벗어나는지 확인하는 변수
     private static boolean isValidMove(int nx, int ny) {
-        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
+        return 0 <= nx && nx < 11 & 0 <= ny && ny < 11;
     }
     
-    // 다음 좌표 결정을 위한 해시맵 생성
+    // Character -> UDLR, int[] -> 이동거리
     private static final HashMap<Character, int[]> location = new HashMap<>();
     
     private static void initLocation() {
@@ -16,39 +16,37 @@ class Solution {
         location.put('L', new int[]{1, 0});
         location.put('R', new int[]{-1, 0});
     }
-    
+        
     public int solution(String dirs) {
         // 메서드 호출
         initLocation();
         
-        // 배열은 음수 인덱스를 사용을 못하기 때문에 양수로 쉬프트
-        int x = 5, y = 5;        
+        // 음수에서 양수로 쉬프트
+        int x = 5, y = 5;
         
-        // 중복된 결과를 제외하려면 HashSet이 효과적
+        // 중복을 체크하기 위해 HashSet 선언
         HashSet<String> answer = new HashSet<>();
         
-        // 주어진 명령어로 움직이면서 좌표 저장
+        // 주어진 명령어로 이동 
         for (int i = 0; i < dirs.length(); i++) {
             int[] offset = location.get(dirs.charAt(i));
             int nx = x + offset[0];
-            int ny = y + offset[1];            
+            int ny = y + offset[1];
             
-            // 벗어난 좌표는 인정하지 않음 
+            // 좌표를 벗어났는지 확인
             if (!isValidMove(nx, ny)) {
                 continue;
             }
             
-            // A에서 B로 간 경우 B에서 A도 추가(총 경로의 개수는 방향성이 없음)
-            // 각 좌표를 정확하게 구분하기 위해 공백을 추가 
+            // A -> B, B -> A 이동 방향 계산
+            // 각 좌표의 정확한 계산을 위해 공백 추가
             answer.add(x + " " + y + " " + nx + " " + ny);
             answer.add(nx + " " + ny + " " + x + " " + y);
-            // 좌표를 이동했으므로 업데이트
+            
+            // 좌표 갱신
             x = nx;
             y = ny;
-            
         }
-        
-        // HashSet의 크기는 2배로 증가하여 2로 나눠줘야 실제 개수가 됨
         return answer.size() / 2;
     }
 }
