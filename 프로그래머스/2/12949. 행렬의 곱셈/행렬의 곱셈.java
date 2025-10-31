529d4737cd88d06df1428f92285e247289d11194
JONGHUKIM <hotsky803@gmail.com>
2025-10-31 14:28:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.72 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,17 +1,17 @@
-// 복습
-
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {              
+    public int[][] solution(int[][] arr1, int[][] arr2) {
         int r1 = arr1.length;
         int c1 = arr1[0].length;
         int r2 = arr2.length;
         int c2 = arr2[0].length;
         
         int[][] answer = new int[r1][c2];
+        // 행렬의 곱은 c1 == r2 ( r1 * c2 의 행렬이 만들어짐 )
         
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c2; j++) {
                 for (int k = 0; k < c1; k++) {
+                    // 첫 번째 열과 두번 째 행이 같아야 곱셈 가능
                     answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
