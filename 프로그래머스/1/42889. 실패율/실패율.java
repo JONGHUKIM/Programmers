497e6e42b5118bdb89359652acbc11bdcf6d4253
JONGHUKIM <hotsky803@gmail.com>
2025-10-31 15:08:35 +0900

    [level 1] Title: 실패율, Time: 10.07 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,32 +1,28 @@
-// 복습
-
 import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        // 플레이어 수 구하기
+        // 각 스테이지 플레이어 수 구하기
         int[] challenger = new int[N + 2];
         for (int stage : stages) {
-            challenger[stage] += 1; // 각 스테이지에 있는 플레이어 
+            challenger[stage] += 1;
         }
         
-        // 실패율을 구하기 위한 변수 선언
         HashMap<Integer, Double> fails = new HashMap<>();
-        double total = stages.length; // 전체 플레이어 수
+        double total = stages.length; // 전체 사용자
         
-        for (int i = 1; i <= N; i++) { // 스테이지는 1부터
-            if (challenger[i] == 0) { // 실패율 0이라면
+        // 실패율 구하기
+        for (int i = 1; i <= N; i++) {
+            if (challenger[i] == 0) {
                 fails.put(i, 0.);
             } else {
-                fails.put(i, challenger[i] / total); // 실패율 구하기
-                total -= challenger[i]; // 다음 스테이지를 위해 현재 스테이지의 인원 감소
+                fails.put(i, challenger[i] / total);
+                total -= challenger[i];
             }
         }
-        
-        // 출력
         return fails.entrySet().stream().sorted((o1, o2) -> 
         o1.getValue().equals(o2.getValue()) ? Integer.compare(o1.getKey(),
-        o2.getKey()) : Double.compare(o2.getValue(),
+        o2.getKey()) : Double.compare(o2.getValue(), 
         o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
-    }
+    }        
 }
