ebdf60593034a06f71cc65734403fb1ea0a81ba5
JONGHUKIM <hotsky803@gmail.com>
2025-11-03 16:06:27 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.75 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
-        // 구하려는 배열 길이 구하기
         int len = (int)(right - left + 1);
         int[] answer = new int[len];
         
-        // 인덱스 선언
         int idx = 0;
-        // 배열의 왼쪽부터 오른쪽까지 모든 인덱스 탐색
         for (long i = left; i <= right; i++) {
-            int row = (int)(i / n); // 2차원 행으로 변환
-            int col = (int)(i % n); // 2차원 열로 변환
+            int row = (int)(i / n);
+            int col = (int)(i % n);
             
-            // 2차원 배열의 규칙, 각 위치의 값은 max(열, 행) + 1
-            // idx++ -> 현재 인덱스에 값을 저장 후 다음 위치로 이동
             answer[idx++] = (int)(Math.max(row, col) + 1);
         }
-        
         return answer;
     }
 }
