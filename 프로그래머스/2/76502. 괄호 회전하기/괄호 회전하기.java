34631584334e825da556ed2c1865103014c102a7
JONGHUKIM <hotsky803@gmail.com>
2025-11-04 16:13:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 47.61 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,39 +1,37 @@
 import java.util.*;
+// 어렵다
 
-// {}, (), []을 HashMap에 저장한다
-// A:for 문을 이용해서 괄호를 비교한다
-// 스택을 push한다
-// 스택을 pop한다
 
 class Solution {
     public int solution(String s) {
+        // 해쉬맵 선언
         HashMap<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
         
-        // 원본 문자열 길이 저장
+        // S의 길이만큼 선언
         int n = s.length();
-        // 원본 문자열 뒤에 원본 문자열을 이어 붙여서 2번 나오도록 함
-        s += s;            
+        // 이어붙이기를 위해 s += s
+        s += s;
         int answer = 0;
         
         A:for (int i = 0; i < n; i++) {
             ArrayDeque<Character> stack = new ArrayDeque<>();
-            for (int j = i; j < i + n; j++) {
+            for (int j = i; j < i + n; j++){
                 char c = s.charAt(j);
-                // 해시맵안에 해당 키가 없다면 열리는 괄호
+                
                 if (!map.containsKey(c)) {
                     stack.push(c);
                 } else {
                     if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
                         continue A;
-                    }
-                }
+                    }                
+                }   
             }
             if (stack.isEmpty()) {
-                    answer++;
-                }
+                answer++;
+            }
         }
         return answer;
     }
