ca9edfbb6af084deab115379096b3b965a163c60
JONGHUKIM <hotsky803@gmail.com>
2025-11-04 17:05:17 +0900

    [level 2] Title: 짝지어 제거하기, Time: 61.13 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
+        // 스택 선언
         ArrayDeque<Character> stack = new ArrayDeque<>();
-               
-        char[] c = s.toCharArray();
         
         for (int i = 0; i < s.length(); i++) {
-            if (!stack.isEmpty() && stack.peek() == c[i]) {
+            char c = s.charAt(i);
+            // 스택의 맨 위와 값이 같다면 pop
+            if (!stack.isEmpty() && stack.peek() == c) {
                 stack.pop();                
             } else {
-                stack.push(c[i]);
-            }
-                
-           
+                // push
+                stack.push(c);
+            }                           
         }
+        // 스택이 비어있으면 1 아니면 0
         return stack.isEmpty() ? 1 : 0;
     }
 }
