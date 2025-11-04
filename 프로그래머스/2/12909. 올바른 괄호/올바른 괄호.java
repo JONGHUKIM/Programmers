87cd16d4f2238ea9d03e5fb20981f875d33d55f4
JONGHUKIM <hotsky803@gmail.com>
2025-11-04 16:13:40 +0900

    [level 2] Title: 올바른 괄호, Time: 17.72 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,14 +4,18 @@ class Solution {
     boolean solution(String s) {
         ArrayDeque<Character> stack = new ArrayDeque<>();
         
+        // 괄호를 저장할 배열 선언
         char[] a = s.toCharArray();
         for (char c : a) {
+            // c와 '('가 같은지 비교 후 '('라면 push
             if (c == '(') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty() || stack.pop() == c) {
+                // 닫힌 괄호가 들어오면 바로 false;
+                if (stack.isEmpty()) {
                     return false;
                 }
+                stack.pop();
             }
         }
         return stack.isEmpty();
