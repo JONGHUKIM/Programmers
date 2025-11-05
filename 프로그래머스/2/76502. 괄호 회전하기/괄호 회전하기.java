012d3470220a1a7c38b4ef25d7d9e1bab645878b
JONGHUKIM <hotsky803@gmail.com>
2025-11-05 15:07:46 +0900

    [level 2] Title: 괄호 회전하기, Time: 42.95 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,38 +1,32 @@
 import java.util.*;
-// 어렵다
-
 
 class Solution {
     public int solution(String s) {
-        // 해쉬맵 선언
         HashMap<Character, Character> map = new HashMap<>();
         map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
+		map.put('}', '{');
+		map.put(']', '[');
         
-        // S의 길이만큼 선언
         int n = s.length();
-        // 이어붙이기를 위해 s += s
         s += s;
-        int answer = 0;
+        int result = 0;
         
         A:for (int i = 0; i < n; i++) {
-            ArrayDeque<Character> stack = new ArrayDeque<>();
-            for (int j = i; j < i + n; j++){
+          ArrayDeque<Character> stack = new ArrayDeque<>();
+            for (int j = i; j < i + n; j++) {
                 char c = s.charAt(j);
-                
                 if (!map.containsKey(c)) {
                     stack.push(c);
                 } else {
                     if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
                         continue A;
-                    }                
-                }   
-            }
+                    }
+                }
+            }            
             if (stack.isEmpty()) {
-                answer++;
+                result++;
             }
         }
-        return answer;
+        return result;
     }
 }
