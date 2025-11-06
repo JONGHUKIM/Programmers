777a2e1191fe60113190c870e8d1955723a371e6
JONGHUKIM <hotsky803@gmail.com>
2025-11-06 15:31:19 +0900

    [level 2] Title: 주식가격, Time: 25.55 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,33 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        // 스택에 푸쉬하는 값은 주식의 가격이 아닌 주식의 인덱스
+        // 주식의 인덱스를 저장할 스택 선언
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         int n = prices.length;
         int[] answer = new int[n];
         
-        // 최초엔 비교 대상이 없으므로 0 푸쉬
-        stack.push(0);
+        stack.push(0); // 비굣값을 위한 초기 인덱스
         
-        // 인덱스 1번부터 비교
+        // 인덱스와 비교하기 위해 1부터
         for (int i = 1; i < n; i++) {
-            // 스택에 값이 있고 현재 주식가격(i)가 과거 주식가격(prices[stack.peek()])보다 떨어지면 pop
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {                
+            // 현재 주식의 가격과 과거 주식의 가격을 비교
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                // 주식의 인덱스를 j에 저장
                 int j = stack.pop();
-                // 현재 인덱스 i - 과거 인덱스 j
-                // 값이 아닌 인덱스의 간격을 구함
-                answer[j] = i - j;
+                answer[j] = i - j; // 현재 인덱스 - 과거 인덱스 (3 - 2) 1초를 answer에 저장
             }
             stack.push(i);
         }
         
-        // 남아있는 스택 즉, 마지막까지 가격이 안떨어짐
-        while (!stack.isEmpty()) {
-            // 스택은 인덱스를 저장
+        // 스택에 남아있는 숫자 처리
+        while(!stack.isEmpty()) {
             int j = stack.pop();
-            // (5 - 1) - 4 = 0 인덱스의 길이로 계산
+            // (5 - 1) - 3 = 1초로 answer 리턴
             answer[j] = (n - 1) - j;
         }
+        
         return answer;
     }
 }
