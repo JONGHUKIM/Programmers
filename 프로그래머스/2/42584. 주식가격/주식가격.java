aea4be13e785611fe6fc6813007962a16bfbe1d0
JONGHUKIM <hotsky803@gmail.com>
2025-11-07 15:18:05 +0900

    [level 2] Title: 주식가격, Time: 26.10 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,29 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        // 주식의 인덱스를 저장할 스택 선언
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         int n = prices.length;
         int[] answer = new int[n];
         
-        stack.push(0); // 비굣값을 위한 초기 인덱스
+        stack.push(0);
         
-        // 인덱스와 비교하기 위해 1부터
         for (int i = 1; i < n; i++) {
-            // 현재 주식의 가격과 과거 주식의 가격을 비교
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                // 주식의 인덱스를 j에 저장
                 int j = stack.pop();
-                answer[j] = i - j; // 현재 인덱스 - 과거 인덱스 (3 - 2) 1초를 answer에 저장
+                
+                answer[j] = i - j;
             }
             stack.push(i);
-        }
+        }  
         
-        // 스택에 남아있는 숫자 처리
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             int j = stack.pop();
-            // (5 - 1) - 3 = 1초로 answer 리턴
-            answer[j] = (n - 1) - j;
+            answer[j] = (n - j) - 1;
         }
         
         return answer;
