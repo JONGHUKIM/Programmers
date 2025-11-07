47773d06d723e80f0b28ea8fc7067d79c6e7d214
JONGHUKIM <hotsky803@gmail.com>
2025-11-07 15:10:42 +0900

    [level 2] Title: 기능개발, Time: 3.58 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,22 +4,29 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> answer = new ArrayList<>();
         
+        
         for (int i = 0; i < progresses.length; i++) {
-            // 기능을 개발 하는데 며칠이 걸리는지 날짜 구하기
-            double days = (100 - progresses[i]) / (double) speeds[i];
+            // 날짜를 구하기
+            // ex (100 - 93) / 1 = 7
+            double days = (100 - progresses[i]) / (double) speeds[i];            
             int daysUp = (int) Math.ceil(days);
             
-            // 함꼐 배포할 기능의 index 찾기
+            // 같이 배포 될 기능 구하기
             int j = i + 1;
             for (; j < progresses.length; j++) {
+                // ex1) 30 + 7 * 30 = 240 배포 확정
+                // ex2) 55 + 7 * 5 = 90 배포 실패
                 if (progresses[j] + daysUp * speeds[j] < 100) {
                     break;
                 }
             }
             
-            // 이번에 배포할 기능의 개수를 추가
+            // 배포할 기능을 추가 
+            // ex) j = 2, i = 0 --> 2 - 0 = 2
             answer.add(j - i);
             
+            // ex) j = 2 --> 2 - 1 = 1
+            // i = 1 다시 1부터 루프
             i = j - 1;
         }
         
