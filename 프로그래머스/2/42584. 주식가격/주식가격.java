7dd154d3ee01c2cba29b9072a0966dc06167f72c
JONGHUKIM <hotsky803@gmail.com>
2025-11-10 16:38:42 +0900

    [level 2] Title: 주식가격, Time: 25.07 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -8,20 +8,18 @@ class Solution {
         
         stack.push(0);
         
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
                 int j = stack.pop();
-                
-                answer[j] = i - j;
+                answer[j] = i - j;                
             }
             stack.push(i);
-        }  
+        }
         
         while (!stack.isEmpty()) {
             int j = stack.pop();
-            answer[j] = (n - j) - 1;
+            answer[j] = (n - 1) - j;
         }
-        
         return answer;
     }
 }
