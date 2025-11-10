f12c64d58ce87bac6cc6eaeeb1807ba7be2f94fc
JONGHUKIM <hotsky803@gmail.com>
2025-11-10 17:02:33 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.26 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        String[] words = s.split(" ");
-        ArrayList<Integer> nums = new ArrayList();
+        // 스택선언        
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
         int answer = 0;
         
-        for (String word : words) {
-            if (word.equalsIgnoreCase("Z")) {
-                if (!nums.isEmpty()) {
-                    int lastNum = nums.remove(nums.size() - 1 );
-                    answer -= lastNum;
+        // 공백을 제거하고 w에 저장
+        for (String w : s.split(" ")) {
+            if (w.equals("Z")) { // Z가 나오면
+                if (!stack.isEmpty()) {
+                    answer -= stack.pop(); // 직전에 더했던 값을 뺌
                 }
             } else {
-                int numb = Integer.parseInt(word);
-                nums.add(numb);
-                answer += numb;
+                int num = Integer.parseInt(w);
+                stack.push(num);
+                answer += num; // Z가 없다면 누적합
             }
         }
-        
-        return answer;
+        return answer;        
     }
 }
