9a5f9b3f36f5feeea89ddf3bfc0e983b77dd04f7
JONGHUKIM <hotsky803@gmail.com>
2025-11-13 14:30:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.35 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,22 +4,18 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        // put get getOrDefault
         HashMap<String, Integer> map = new HashMap<>();
         
-        // Participant 선수 명단 HashMap에 저장
-        for (String player : participant) {            
-            map.put(player, map.getOrDefault(player, 0) + 1); // 동명이인 계산
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
         }
         
-        // Completion 선수 명단 HashMap에 저장
         for (String player : completion) {
-            map.put(player, map.get(player) - 1); // 모든 키값에 - 1
+            map.put(player, map.get(player) - 1);
         }
         
-        // 키 호출
         for (String key : map.keySet()) {
-            if (map.get(key) != 0) { // 0 아니고 1이 남는다면 완주 못한 선수
+            if (map.get(key) != 0) {
                 answer = key;
                 break;
             }
