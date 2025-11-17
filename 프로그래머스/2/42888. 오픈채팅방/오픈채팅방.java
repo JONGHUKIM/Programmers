fd19a67e596b3822dee75590c9a9eb72ab3b9f1b
WOOJOO <hotsky803@gmail.com>
2025-11-17 16:23:10 +0900

    [level 2] Title: 오픈채팅방, Time: 123.19 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,21 +3,20 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         ArrayList<String> answer = new ArrayList<>();
+        
         HashMap<String, String> idMap = new HashMap<>();
         
-        // 최신 기록을 저장
         for (String str : record) {
             StringTokenizer st = new StringTokenizer(str);
             String cmd = st.nextToken();
             
             if (cmd.equals("Enter") || cmd.equals("Change")) {
                 String id = st.nextToken();
-                String name = st.nextToken();
+                String name = st.nextToken();                
                 idMap.put(id, name);
             }
         }
         
-        // 최신 기록을 순회하며 메시지 출력
         for (String str : record) {
             StringTokenizer st = new StringTokenizer(str);
             String cmd = st.nextToken();
