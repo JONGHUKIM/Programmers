8169fc0d72815c5803d302072811eee77d3064e1
WOOJOO <hotsky803@gmail.com>
2025-11-17 16:00:23 +0900

    [level 2] Title: 할인 행사, Time: 79.90 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,22 +4,22 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        HashMap<String, Integer> wish = new HashMap<>();
-        // 원하는 품목 및 개수 저장
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        
         for (int i = 0; i < want.length; i++) {
-            wish.put(want[i], number[i]);
+            wantMap.put(want[i], number[i]);
         }
         
+        // 연속된 10일 (슬라이딩 윈도우)
         for (int i = 0; i < discount.length - 9; i++) {
-            HashMap<String, Integer> dis10days = new HashMap<>();
+            HashMap<String, Integer> discount10 = new HashMap<>();
             
             for (int j = i; j < i + 10; j++) {
-                if (wish.containsKey(discount[j])) {
-                    dis10days.put(discount[j], dis10days.getOrDefault(discount[j], 0) + 1);
+                if (wantMap.containsKey(discount[j])) {
+                    discount10.put(discount[j], discount10.getOrDefault(discount[j], 0) + 1);
                 }
             }
-            
-            if (wish.equals(dis10days)) {
+            if (wantMap.equals(discount10)) {
                 answer++;
             }
         }
