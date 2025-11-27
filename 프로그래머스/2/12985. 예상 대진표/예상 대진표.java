e47e2c97b09c67c28ca67b8efea15080c384f588
WOOJOO <hotsky803@gmail.com>
2025-11-27 16:18:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -5,8 +5,9 @@ class Solution
         int answer;
 
         for (answer = 0; a != b; answer++) {
+            // 부모 = (자식 + 1) / 2
             a = (a + 1) / 2;
-            b = (b + 1) / 2;
+            b = (b + 1) / 2;        
         }
 
         return answer;
