0b8649cce9511586855f8fd42f3b7368f32b4a26
WOOJOO <hotsky803@gmail.com>
2025-11-30 14:31:50 +0900

    [level 0] Title: 약수 구하기, Time: 0.15 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -4,6 +4,7 @@ class Solution {
     public int[] solution(int n) {
         ArrayList<Integer> divisors = new ArrayList<>();
         
+        // 약수 공식
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 divisors.add(i);
@@ -11,6 +12,7 @@ class Solution {
         }
         
         int[] answer = new int[divisors.size()];
+        
         for (int i = 0; i < divisors.size(); i++) {
             answer[i] = divisors.get(i);
         }
