12357c8eaa94ece3e45e9e0299f1142596b2ee13
WOOJOO <hotsky803@gmail.com>
2025-11-30 14:30:58 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,10 +1,13 @@
 class Solution {
     public long solution(String numbers) {
+        // 영어 넘버 하드코딩
         String[] engNumbs = {"zero", "one", "two", "three", "four", "five", "six", 
                            "seven", "eight", "nine"};
+        // 숫자 하드코딩
         String[] numbs = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         
         for (int i = 0; i < engNumbs.length; i++) {
+            // 영어 -> 숫자로 변경
             numbers = numbers.replace(engNumbs[i], numbs[i]);
         }
         
