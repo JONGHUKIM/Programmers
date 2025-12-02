e336ffbabff3e60207254e44f5473bf34e4e9796
WOOJOO <hotsky803@gmail.com>
2025-12-02 09:32:56 +0900

    [level 2] Title: 전화번호 목록, Time: 324.40 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,23 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호를 저장할 map 선언
-        Map<String, Boolean> map = new HashMap<>();
-                
-        // map에 전화번호 저장
-        for (String num : phone_book) {
-            map.put(num, true);
-        }
+        // 정렬
+        Arrays.sort(phone_book);
         
-        // 접두어 확인
-        for (int i = 0; i < phone_book.length; i++) {
-            // j < phone_book[i].length() -> 접두어가 아닌 자기 자신 전체는 검사에서 제외
-            for (int j = 1; j < phone_book[i].length(); j++) {
-                // phone_book[i]의 0부터 j까지 자른 문자열이 map에 있는지 확인
-                if (map.containsKey(phone_book[i].substring(0, j))) {
-                    return false;
-                }
-            }
+        // i와 i+1만 비교
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            // 뒷 번호(i+1)가 앞 번호(i)로 시작?
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }                
         }
         
         return true;
