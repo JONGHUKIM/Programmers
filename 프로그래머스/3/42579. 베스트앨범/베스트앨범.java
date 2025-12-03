6dec4fb157559d38a6adde078815fe3cbb70f3eb
WOOJOO <hotsky803@gmail.com>
2025-12-03 11:29:19 +0900

    [level 3] Title: 베스트앨범, Time: 12.50 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,12 +1,11 @@
-// 복습
-
 import java.util.*;
 import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, ArrayList<int[]>> genreMap = new HashMap<>();
-        HashMap<String, Integer> playMap = new HashMap<>();
+                
+        Map<String, ArrayList<int[]>> genreMap = new HashMap<>();
+        Map<String, Integer> playMap = new HashMap<>();
         
         for (int i = 0; i < genres.length; i++) {
             String genre = genres[i];
@@ -21,35 +20,24 @@ class Solution {
             playMap.put(genre, playMap.getOrDefault(genre, 0) + play);
         }
         
-        ArrayList<Integer> answerList = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         
-        Stream<Map.Entry<String, Integer>> sortedGenre =
-            playMap.entrySet().stream().sorted((o1, o2) -> 
-                            Integer.compare(o2.getValue(), o1.getValue()));
+        Stream<Map.Entry<String, Integer>> sortedGenre = 
+            playMap.entrySet().stream()
+                   .sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
         
         sortedGenre.forEach(entry -> {
-            
-            Stream<int[]> sortedSong = 
-                genreMap.get(entry.getKey()).stream().sorted((o1, o2) -> {
-                
-                if (o1[1] == o2[1]) {
-                    return Integer.compare(o1[0], o2[0]);
-                }
-                
-                return Integer.compare(o2[1], o1[1]);
-                
-            }).limit(2);
-            
-            sortedSong.forEach(song -> answerList.add(song[0]));
-            
-        });        
+            Stream<int[]> sortedSongs = 
+                genreMap.get(entry.getKey()).stream()
+                        .sorted((o1, o2) -> {
+                            if (o1[1] == o2[1]) {
+                                return Integer.compare(o1[0], o2[0]);
+                            }
+                            return Integer.compare(o2[1], o1[1]);
+                        }).limit(2);
+            sortedSongs.forEach(song -> answer.add(song[0]));
+        });
         
-        int[] answer = new int[answerList.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = answerList.get(i);
-        }
-        
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
