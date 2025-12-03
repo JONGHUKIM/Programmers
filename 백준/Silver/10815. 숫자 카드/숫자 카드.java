a357a7bdb3d675cd0a354f5d91d6252ed39e5e4e
WOOJOO <hotsky803@gmail.com>
2025-12-03 13:31:12 +0900

    [Silver V] Title: 숫자 카드, Time: 852 ms, Memory: 151680 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =
+            new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = 
+            new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < N; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            
+            map.put(key, 1);
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            
+            sb.append(map.getOrDefault(target, 0)).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+        
+    }
+}
