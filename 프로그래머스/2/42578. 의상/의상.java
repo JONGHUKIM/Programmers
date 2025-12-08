a53746fb1273ac0181129758cdd88b0a973436de
WOOJOO <hotsky803@gmail.com>
2025-12-08 11:50:18 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        int answer = 1;
         
-        Map<String, Integer> wear = new HashMap<>();
+        HashMap<String, Integer> wear = new HashMap<>();
         
         for (int i = 0; i < clothes.length; i++) {
             String type = clothes[i][1];
             
             wear.put(type, wear.getOrDefault(type, 0) + 1);
-        }        
-        
-        int answer = 1;
+        }
         
         for (int count : wear.values()) {
             answer *= (count + 1);
         }
+        
+        // 하나 꼭 빼기
         return answer - 1;
     }
 }
