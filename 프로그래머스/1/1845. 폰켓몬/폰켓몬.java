ae5b1bac8938ab0f7af9ee4cf85397565642bf1f
WOOJOO <hotsky803@gmail.com>
2025-12-08 11:37:35 +0900

    [level 1] Title: 폰켓몬, Time: 6.19 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 폰켓몬을 담을 hashSet 선언
-        // 중복되는 폰켓몬을 제외
-        HashSet<Integer> hash = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         
-        // 가져갈 수 있는 폰켓몬 
         int max = nums.length / 2;
         
-        // hash에 값을 채우면서 중복 제거
         for (int num : nums) {
-            hash.add(num);
+            set.add(num);
         }
         
-        // 종류 수 vs 가져갈 수 있는 수 중 작은 값
-        return Math.min(hash.size(), max);
+        return Math.min(set.size(), max);
     }
 }
