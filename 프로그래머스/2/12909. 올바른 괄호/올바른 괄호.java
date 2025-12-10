fc117222f036e3e47d6a8049983bd2857c9f2313
WOOJOO <hotsky803@gmail.com>
2025-12-10 09:05:08 +0900

    [level 2] Title: 올바른 괄호, Time: 13.87 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -10,12 +10,13 @@ class Solution {
             if (c == '(') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty() || stack.pop() == c) {
+                if (stack.isEmpty()) {
                     return false;
                 }
+                stack.pop();
             }
         }
-
+        
         return stack.isEmpty();
     }
 }
