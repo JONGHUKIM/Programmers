27ecdddfb69a030d870892ad1ee60c8f92b3c7b3
WOOJOO <hotsky803@gmail.com>
2025-12-19 16:56:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.54 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,21 +3,17 @@ import java.util.*;
 class Solution {
     
     static final int[] dx = {1, -1, 0, 0};
-    static final int[] dy = {0, 0, 1, -1};
+    static final int[] dy = {0, 0, -1, 1};
     
-    boolean[][] visited;
+    private boolean[][] visited;
+    private int[][] maps;    
+    private int n,m;
     
-    public int solution(int[][] maps) {
-        
+    private int bfs() {
         ArrayDeque<int[]> que = new ArrayDeque<>();
         
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        visited = new boolean[n][m];
-        
-        que.add(new int[]{0, 0, 1});
         visited[0][0] = true;
+        que.add(new int[]{0, 0, 1});
         
         while(!que.isEmpty()) {
             int[] cur = que.poll();
@@ -29,28 +25,31 @@ class Solution {
                 return dist;
             }
             
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                    continue;
-                }
-                
-                if (maps[nx][ny] == 0) {
-                    continue;
-                }
-                
-                if (visited[nx][ny]) {
-                    continue;
-                }
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (maps[nx][ny] == 0) continue;
+                if (visited[nx][ny]) continue;
                 
                 visited[nx][ny] = true;
-                
                 que.add(new int[]{nx, ny, dist + 1});
-                
-            }
-        }        
+            }            
+        }
         return -1;
+    }
+    
+    public int solution(int[][] maps) {
+        
+        this.maps = maps;
+        
+        n = maps.length;
+        m = maps[0].length;
+        
+        visited = new boolean[n][m];
+        maps = new int[n][m];
+        
+        return bfs();
     }
 }
