efd68ccb41fffca8446d351a3fc69107ae971b0d
WOOJOO <hotsky803@gmail.com>
2025-12-25 15:53:02 +0900

    [level 1] Title: 폰켓몬, Time: 6.80 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        // 중복 제거 셋 선언
         HashSet<Integer> set = new HashSet<>();
         
         int max = nums.length / 2;
