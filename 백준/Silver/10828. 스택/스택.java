a33ee3acfd12a962ff4a39ffb9a2bdeb5c0e4ccd
WOOJOO <hotsky803@gmail.com>
2026-01-03 12:11:23 +0900

    [Silver IV] Title: 스택, Time: 136 ms, Memory: 16916 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -15,23 +15,20 @@ public class Main {
             String cmd = br.readLine();
             
             if (cmd.startsWith("push")) {
-                int x = Integer.parseInt(cmd.substring(5));
-                // 인덱스가 -1에서 0이 되어 stack[0] = 2가 됨
-                // 또 다음 추가가 되면 stack[1]
-                stack[++top] = x;
+                int push = Integer.parseInt(cmd.substring(5));
+                stack[++top] = push;
             } else if (cmd.equals("pop")) {
                 if (top == -1) sb.append(-1).append("\n");
-                else sb.append(stack[top--]).append('\n');                
-            } else if (cmd.equals("top")) {
-                if (top == -1) sb.append(-1).append("\n");                
-                else sb.append(stack[top]).append("\n");
+                else sb.append(stack[top--]).append("\n");
             } else if (cmd.equals("size")) {
                 sb.append(top + 1).append("\n");
-            } else if (cmd.equals("empty")) {
+            } else if(cmd.equals("empty")) {
                 sb.append(top == -1 ? 1 : 0).append("\n");
+            } else if(cmd.equals("top")) {
+                if (top == -1) sb.append(-1).append("\n");
+                else sb.append(stack[top]).append("\n");
             }
         }
-       
         System.out.println(sb.toString());
         br.close();
     }
