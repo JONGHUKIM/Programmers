83077dc2922443f77c01e7e1ee38314df26a9e28
WOOJOO <hotsky803@gmail.com>
2026-01-03 10:00:51 +0900

    [Silver IV] Title: 문자열, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.java
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -7,6 +7,7 @@ public class Main {
             new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st =
             new StringTokenizer(br.readLine());
+        
         String a = st.nextToken();
         String b = st.nextToken();
         
@@ -15,19 +16,10 @@ public class Main {
         
         int answer = Integer.MAX_VALUE;
         
-        // a의 시작점을 옮기면서 비교
-        // i = 0일 때
-        // B : adbdfsb
-        // A : abab
-        
-        // i = 1일 때
-        // B : adbdfsb
-        // A :  abab
         for (int i = 0; i <= lenB - lenA; i++) {
             int diff = 0;
             for (int j = 0; j < lenA; j++) {
-                // 현재 위치의 a문자와 b의 문자를 비교
-                if (a.charAt(j) != b.charAt(i + j)) {
+                if (a.charAt(j) != b.charAt(i+j)) {
                     diff++;
                 }
             }
