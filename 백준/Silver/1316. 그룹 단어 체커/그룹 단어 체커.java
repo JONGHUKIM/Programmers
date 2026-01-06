52dacf192a44d758a15caed549cd5d6198624411
WOOJOO <hotsky803@gmail.com>
2026-01-06 10:24:03 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -5,32 +5,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
-        
-        int answer = 0;
+                        
         int n = Integer.parseInt(br.readLine());
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            
-            boolean[] check = new boolean[26];
-            boolean ok = true;
             char prev = 0;
+            boolean ok = true;
+            boolean[] alpha = new boolean[26];
             
             for (int j = 0; j < s.length(); j++) {
                 char cur = s.charAt(j);
                 
                 if (cur != prev) {
-                    if (check[cur - 'a']) {
+                    if (alpha[cur - 'a']) {
                         ok = false;
                         break;
                     }
-                    check[cur -'a'] = true;
+                    alpha[cur - 'a'] = true;
                     prev = cur;
                 }
             }
+            
             if (ok) answer++;
         }
         
         System.out.println(answer);
+        br.close();
     }
 }
