8c06e289f6dcf65a9009eed4e4137e612deded5c
WOOJOO <hotsky803@gmail.com>
2026-01-06 10:34:23 +0900

    [Bronze I] Title: 단어 공부, Time: 244 ms, Memory: 21120 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -7,27 +7,24 @@ public class Main {
             new BufferedReader(new InputStreamReader(System.in));
         
         String s = br.readLine().toUpperCase();
-        
+        char answer = '?';
         int[] count = new int[26];
+        int max = Integer.MIN_VALUE;
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             count[c - 'A']++;
         }
         
-        int max = Integer.MIN_VALUE;
-        char answer = '?';
-        
-        for (int i = 0; i < 26; i++){
-            if(count[i] > max) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
                 max = count[i];
-                answer = (char)(i + 'A');
+                answer = (char) (i + 'A');
             } else if (count[i] == max) {
                 answer = '?';
             }
         }
         
         System.out.println(answer);
-        br.close();
     }
 }
