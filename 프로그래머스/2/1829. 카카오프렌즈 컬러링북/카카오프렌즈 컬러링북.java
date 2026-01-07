ac5217e0de8bdec9d62628077f7024fc66079d78
WOOJOO <hotsky803@gmail.com>
2026-01-07 15:57:44 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 8.58 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -1,31 +1,31 @@
+import java.util.*;
+
 class Solution {
     
     private int[] dx = {1, -1, 0, 0};
     private int[] dy = {0, 0, 1, -1};
     
-    private int numberOfArea, maxSizeOfOneArea, m, n, tempSize;
-    
     private int[][] map;
     private boolean[][] visited;
     
-    private void dfs(int x, int y) {
+    private int numberOfArea, maxSizeOfOneArea, m, n, tempSize;
+    
+    private void dfs (int x, int y) {
         visited[x][y] = true;
-        
         tempSize++;
         
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;            
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
             if (visited[nx][ny]) continue;
             if (map[x][y] == map[nx][ny]) {
-                dfs(nx, ny);
+                dfs(nx, ny);                
             }
-            
         }
-        
     }
+        
     
     public int[] solution(int m, int n, int[][] picture) {
         numberOfArea = 0;
@@ -39,15 +39,17 @@ class Solution {
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                
                 if (map[i][j] != 0 && !visited[i][j]) {
                     numberOfArea++;
                     
                     tempSize = 0;
                     
-                    dfs(i, j);     
+                    dfs(i, j);
                     
                     maxSizeOfOneArea = Math.max(maxSizeOfOneArea, tempSize);
                 }
+                
             }
         }
 
