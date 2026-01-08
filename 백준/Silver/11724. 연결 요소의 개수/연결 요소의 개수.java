7b30eee9b4a90d913ccea934334d96a02aadcc9a
WOOJOO <hotsky803@gmail.com>
2026-01-08 15:54:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 612 ms, Memory: 141812 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,44 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {  
+public class Main {
     
-    private static int m, n;
-    private static ArrayList<Integer> graph[];
+    private static int n, m;
     private static boolean[] visited;
+    private static ArrayList<Integer> graph[];
     
-    private static void dfs (int x) {
+    private static void dfs(int x) {
         visited[x] = true;
         
         for (int next : graph[x]) {
-            if(!visited[next]){
+            if (!visited[next]) {
                 dfs(next);
             }
         }
     }
-        
     
     public static void main(String[] args) throws IOException {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw =
-            new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st =
-            new StringTokenizer(br.readLine());
+                new StringTokenizer(br.readLine());
+        
+        int count = 0;
         
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        int count = 0;
         
         visited = new boolean[n + 1];
         graph = new ArrayList[n + 1];
         
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
         
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
+            
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             
@@ -52,9 +51,7 @@ public class Main {
                 count++;
             }
         }
-        bw.write(String.valueOf(count));
-        bw.flush();
-        bw.close();
-        br.close();
+        
+        System.out.println(count);
     }
 }
