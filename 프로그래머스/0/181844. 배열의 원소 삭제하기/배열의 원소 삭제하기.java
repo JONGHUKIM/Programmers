aee3be2624dca1d5fc9f9f17c3fd7a413bdce6ef
WOOJOO <hotsky803@gmail.com>
2026-01-26 19:08:42 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 3.19 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -4,7 +4,6 @@ class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
         ArrayList<Integer> answer = new ArrayList<>();
         
-        // 중복된 배열 제거를 위한 HashSet 선언
         HashSet<Integer> deleteSet = new HashSet<>();
         for (int x : delete_list) {
             deleteSet.add(x);
@@ -16,6 +15,7 @@ class Solution {
             }
         }
         
+        // 스트림
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
